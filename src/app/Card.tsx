e574a63683f766f37a3bdd32cdb466cbc8cb954f
linga-007 <lingesh480500@gmail.com>
2024-01-20 00:23:13 +0530

    designed lhs of the page

---
+++ src/app/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='h-[170px] w-[100%] pl-5 pr-5 gap-4 flex flex-col'>
+        <div className='flex  justify-between'>
+            <div className='w-8 h-8 bg-zinc-600 rounded-full'></div>
+            <h2>60 min</h2>
+        </div>
+        <h2>Weekly Meeting . 10 AM</h2>
+        <h2 className='line-clamp-1'>Jessica Anderson,Julia Foster,Mohammed ashif</h2>
+        <div className='w-full h-[1px] bg-zinc-500'></div>
+        
+    </div>
+  )
+}
+
+export default Card
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import linga from "../app/lingaphoto.jpg";
+import Card from "./Card";
 
 export default function Home() {
   return (
@@ -7,11 +8,11 @@ export default function Home() {
       <div className="flex-shrink-0 h-[150px] xs:h-[75px] bg-black">
         <div className="container mx-auto h-[50px] flex items-center">
           <nav className="flex flex-row w-[50%] mx-auto justify-evenly pt-2 text-white">
-            <button className="hover:text-orange-500 h-8">Dashboard</button>
-            <button className="hover:text-orange-500 h-8">Events</button>
-            <button className="hover:text-orange-500 h-8">My Requests</button>
-            <button className="hover:text-orange-500 h-8">Company news</button>
-            <button className="hover:text-orange-500 h-8">Tasks</button>
+            <button className="hover:text-orange-600 h-8">Dashboard</button>
+            <button className="hover:text-orange-600 h-8">Events</button>
+            <button className="hover:text-orange-600 h-8">My Requests</button>
+            <button className="hover:text-orange-600 h-8">Company news</button>
+            <button className="hover:text-orange-600 h-8">Tasks</button>
           </nav>
           <div className="flex flex-row text-white">
             <h2 className="pt-7">Llyod Robinson</h2>
@@ -24,16 +25,53 @@ export default function Home() {
             ></Image>
           </div>
         </div>
-        <div className=" h-[60px] mt-10 text-white flex ">
-          <div className="mx-10 font-serif text-3xl flex justify-start">
+        <div className=" h-[60px] mt-10 text-white flex justify-between ">
+          <div className="mx-10 font-serif text-3xl pr-3 flex justify-start">
             Good Morning, Lloyd
           </div>
-          
+          <div className="w-[500px] flex justify-evenly">
+            <button className="bg-transparent 0 w-48 h-10 text-zinc-300 font-light border border-zinc-300 rounded-full">
+              New Request
+            </button>
+            <button className="bg-orange-600 0 w-48 h-10 text-sm text-white-700 font-light  rounded-full">
+              Schedule Meeting
+            </button>
+          </div>
         </div>
       </div>
-      <div className="flex-grow container mx-auto p-4">
-        {/* Add your content here */}
-        <p>Hello, world!</p>
+      <div className="flex-grow container bg-zinc-300 mx-auto p-4 flex flex-row">
+        <div className="h-full w-[30%] flex mx-5 gap-6 flex-col ">
+          <div className="h-[150px] rounded-3xl p-5  flex flex-col bg-black">
+            <div className="pb-5">Monthly retrospective</div>
+            <div className="flex justify-between">
+              <h2>In 19 min, 10:00 AM</h2>
+              <h2>35 min</h2>
+            </div>
+            <div className="flex pt-5 justify-between">
+              <div className="w-8 h-8  bg-zinc-600 rounded-full"></div>
+              <button className="bg-white text-black h-8  w-56 rounded-xl">
+                Join now
+              </button>
+            </div>
+          </div>
+          <div className="bg-white h-[500px]  p-5 w-[100%] gap-3 text-black flex flex-col rounded-3xl">
+            <div className="flex justify-between">
+              <h2 className="font-semibold">Upcoming Events</h2>
+              <div className="bg-blue-600 text-xm  text-white h-6 w-16 pl-2 rounded-3xl">
+                2 new
+              </div>
+            </div>
+            <div className="font-serif">March 27</div>
+            <Card/>
+            <Card/>
+            <Card/>
+           
+          </div>
+        </div>
+        <div className="h-[75%] w-[70%] flex mx-5 flex-col bg-black">
+          <h2 className="">hello</h2>
+          <h2>hello</h2>
+        </div>
       </div>
     </main>
   );
